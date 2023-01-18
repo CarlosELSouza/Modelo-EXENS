5aa25d3edb2ce58a9d2ba6f6783d866af29fc4e4
Carlos Eduardo Souza <88462882+CarlosELSouza@users.noreply.github.com>
2023-01-18 12:28:05 -0300

    Update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import '../styles/home.css'
 import exens from '../../src/assets/images/EXENS-3.png'
 import { motion } from 'framer-motion'
 import { Link } from 'react-scroll'
+import pc from '../../src/assets/images/pc.svg'
+import exensvazado from '../../src/assets/images/exens-vaz.png'
 
 const Home = () => {
   return (
@@ -39,19 +41,17 @@ const Home = () => {
         animate={{ x: 0 }}
         transition={{ duration: 1.5 }}
       >
-        <h1>Onde a informação é nossa matéria-prima</h1>
-        <h4>Desde 2003 proporcionando soluções para nossos clientes.</h4>
-        <button className='btn__more'>Saiba mais</button>
+        <h1>A EXENS</h1>
+        <p>A EXENS é uma empresa de TI, especializada em inovação, graças aos investimentos constantes em pesquisa e desenvolvimento, que nos permitem entregar tecnologia de ponta aos nossos clientes e ir muito além do ERP.</p>
       </motion.div>
       <div className="child2-about">
-        <motion.div className="img-container"
+        <motion.div className="img-container-about"
           initial={{ x: 500 }}
           animate={{ x: 0 }}
           transition={{ duration: 1.5 }}
           whileHover={{ scale: 1.1}}
         >
-          
-          <img src={exens} alt=""/>
+          <img src={exensvazado} alt="" />
         </motion.div>
       </div>
     </div>
