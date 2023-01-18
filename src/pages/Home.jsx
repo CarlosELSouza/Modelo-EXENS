52d691f9a071193ab04d5ab0be5a3c27e21d8e58
Carlos Eduardo Souza <88462882+CarlosELSouza@users.noreply.github.com>
2023-01-18 17:59:41 -0300

    Initializing products section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import '../styles/home.css'
-import exens from '../../src/assets/images/EXENS-3.png'
+import exens from '../../src/assets/images/exens-sombra.png'
 import { motion } from 'framer-motion'
 import { Link } from 'react-scroll'
 import AOS from 'aos';
@@ -41,48 +41,69 @@ const Home = () => {
         </motion.div>
       </div>
     </div>
-
-    <div className="about" data-aos="fade-right">
-      <motion.div
-        className="child1-about"
-        initial={{ x: -500 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 1.5 }}
-      >
-        <h1>A EXENS</h1>
-        <p>A EXENS é uma empresa de TI, especializada em inovação, graças aos investimentos constantes em pesquisa e desenvolvimento, que nos permitem entregar tecnologia de ponta aos nossos clientes e ir muito além do ERP.</p>
-      </motion.div>
-      <div className="child2-about">
-        <motion.div className="img-container-about"
-          initial={{ x: 500 }}
+    <div className="about__us-section">
+      <div className="about" data-aos="fade-right">
+        <motion.div
+          className="child1-about"
+          initial={{ x: -500 }}
           animate={{ x: 0 }}
           transition={{ duration: 1.5 }}
-          whileHover={{ scale: 1.1}}
         >
-          <img src={exensvazado} alt="" />
+          <h1>A EXENS</h1>
+          <p>A EXENS é uma empresa de TI, especializada em inovação, graças aos investimentos constantes em pesquisa e desenvolvimento, que nos permitem entregar tecnologia de ponta aos nossos clientes e ir muito além do ERP.</p>
         </motion.div>
+        <div className="child2-about">
+          <motion.div className="img-container-about"
+            initial={{ x: 500 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 1.5 }}
+            whileHover={{ scale: 1.1}}
+          >
+            <img src={exensvazado} alt="" />
+          </motion.div>
+        </div>
+      </div>
+      <div className="about__details">
+        <div className="item"  data-aos="fade-right">
+          <i className="ri-building-line"></i>
+          <h2>Pioneirismo</h2>
+          <h4>Uma das primeiras empresas de Franca-SP a ter um emissor de NFe.</h4>
+        </div>
+        <div className="item"  data-aos="fade-up">
+          <i className="ri-macbook-line"></i>
+          <h2>Multiplataforma</h2>
+          <h4>Desde aplicativo web até aplicativo Offline para vendas.</h4>
+        </div>
+        <div className="item"  data-aos="fade-left">
+          <i className="ri-global-line"></i>
+          <h2>Sempre Atualizado</h2>
+          <h4>Nossa equipe está constantemente trabalhando em melhorias para o ecosistema.</h4>
+        </div>
+      </div>
+      <div className="products__click">
+        <Link to='our__products'>
+          <button data-aos="fade-up" className='btn__products'>Conheça nossos produtos</button>
+        </Link>
       </div>
     </div>
-
-    <div className="about__details">
-      <div className="item"  data-aos="fade-right">
-        <i className="ri-building-line"></i>
-        <h2>Pioneirismo</h2>
-        <h4>Uma das primeiras empresas de Franca-SP a ter um emissor de NFe.</h4>
+    <div className="our__products">
+      <div className="card__container">
+        <div className="card__item">
+          <p>1</p>
+        </div>
+        <div className="card__item">
+          <p>2</p>
+        </div>
+        <div className="card__item">
+          <p>3</p>
+        </div>
+        <div className="card__item">
+          <p>4</p>
+        </div>
+        <div className="card__item">
+          <p>5</p>
+        </div>
       </div>
-      <div className="item"  data-aos="fade-up">
-        <i className="ri-macbook-line"></i>
-        <h2>Multiplataforma</h2>
-        <h4>Desde aplicativo web até aplicativo Offline para vendas.</h4>
-      </div>
-      <div className="item"  data-aos="fade-left">
-        <i className="ri-global-line"></i>
-        <h2>Sempre Atualizado</h2>
-        <h4>Nossa equipe está constantemente trabalhando em melhorias para o ecosistema.</h4>
-      </div>
-    </div>
-    <div className="products__click">
-      <button data-aos="fade-up">Conheça nossos produtos</button>
     </div>
   </section>
   )
