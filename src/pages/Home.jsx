af084635660238edea794d79c9324a36a53b04ff
Carlos Eduardo Souza <88462882+CarlosELSouza@users.noreply.github.com>
2023-01-18 11:30:56 -0300

    Starting hero-section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import '../styles/home.css'
+import exens from '../../src/assets/images/EXENS-3.png'
+import { motion } from 'framer-motion'
 
 const Home = () => {
   return <section className='hero__section-bg'>
     <div className="hero__section">
       <div className="child1">
-        <h3>Onde a informação é nossa matéria-prima</h3>
+        <h1>Onde a informação é nossa matéria-prima</h1>
+        <h4>Desde 2003 proporcionando soluções para nossos clientes.</h4>
+        <button className='btn__more'>Saiba mais</button>
       </div>
       <div className="child2">
-        <h3>Desde 2003 proporcionando soluções para nossos clientes.</h3>
-        <button className='btn__more'>Saiba mais</button>
+        <motion.div className="img-container"
+          initial={{ x: 500 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 1.5 }}
+          whileHover={{ scale: 1.1}}>
+          
+          <img src={exens} alt=""/>
+        </motion.div>
       </div>
     </div>
   </section>
