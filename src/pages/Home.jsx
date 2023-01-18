e8cc749aac6638c66ae48ef8b026e9f09c799c1e
Carlos Eduardo Souza <88462882+CarlosELSouza@users.noreply.github.com>
2023-01-18 12:00:27 -0300

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,27 +2,61 @@ import React from 'react'
 import '../styles/home.css'
 import exens from '../../src/assets/images/EXENS-3.png'
 import { motion } from 'framer-motion'
+import { Link } from 'react-scroll'
 
 const Home = () => {
-  return <section className='hero__section-bg'>
+  return (
+  <section className='hero__section-bg'>
     <div className="hero__section">
-      <div className="child1">
+      <motion.div
+        className="child1"
+        initial={{ x: -500 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 1.5 }}
+      >
         <h1>Onde a informação é nossa matéria-prima</h1>
         <h4>Desde 2003 proporcionando soluções para nossos clientes.</h4>
-        <button className='btn__more'>Saiba mais</button>
-      </div>
+        <Link to="about">
+          <button className='btn__more'>Saiba mais</button>
+        </Link>
+      </motion.div>
       <div className="child2">
         <motion.div className="img-container"
           initial={{ x: 500 }}
           animate={{ x: 0 }}
           transition={{ duration: 1.5 }}
-          whileHover={{ scale: 1.1}}>
+          whileHover={{ scale: 1.1}}
+        >
+          
+          <img src={exens} alt=""/>
+        </motion.div>
+      </div>
+    </div>
+    <div className="about">
+      <motion.div
+        className="child1-about"
+        initial={{ x: -500 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 1.5 }}
+      >
+        <h1>Onde a informação é nossa matéria-prima</h1>
+        <h4>Desde 2003 proporcionando soluções para nossos clientes.</h4>
+        <button className='btn__more'>Saiba mais</button>
+      </motion.div>
+      <div className="child2-about">
+        <motion.div className="img-container"
+          initial={{ x: 500 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 1.5 }}
+          whileHover={{ scale: 1.1}}
+        >
           
           <img src={exens} alt=""/>
         </motion.div>
       </div>
     </div>
   </section>
+  )
 }
 
 export default Home
